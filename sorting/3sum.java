73a48d1e80eeee879db97196af4c40257f322072
Kartik Maurya <112011936+kartik676@users.noreply.github.com>
2026-09-02 23:00:25 +0530

    LC-3sum [Runtime: 34 ms; Memory: 58.9 MB]

--- sorting/3sum.java
+++ sorting/3sum.java
@@ -3,21 +3,32 @@ class Solution {
         Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<>();
 
-        for(int i=0;i<nums.length-2;i++){
-        if (i > 0 && nums[i] == nums[i - 1]) continue; //skip duplicatess
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue; //skip duplicatess
 
-        int l = i+1;
-        int r=nums.length-1;
+            int l = i + 1;
+            int r = nums.length - 1;
 
-        while(l<r){
-            int sum = nums[i]+nums[l]+nums[r];
-                if(sum==0){
-                    result.add(Array.asList(nums[i],nums[l],nums[r]));
-                    
-                }else if(sum<0){
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    while (l < r && nums[l] == nums[l + 1])
+                        l++;
+                    while (l < r && nums[r] == nums[r - 1])
+                        r--;
 
+                    l++;
+                    r--;
+                } else if (sum < 0) {
+                                    l++;
+                }else{
+                    r--;
                 }
-        }
+            }
 
-    }}
+        }
+        return result;
+    }
 }
