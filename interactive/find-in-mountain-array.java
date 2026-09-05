549d5cb50ef72c571b1c0826ed738f05a6086ba6
Kartik Maurya <112011936+kartik676@users.noreply.github.com>
2026-09-05 17:01:29 +0530

    LC-find-in-mountain-array [Runtime: N/A; Memory: N/A]

--- interactive/find-in-mountain-array.java
+++ interactive/find-in-mountain-array.java
@@ -1,81 +1,54 @@
-/**
- * // This is MountainArray's API interface.
- * // You should not implement it, or speculate about its implementation
- * interface MountainArray {
- *     public int get(int index) {}
- *     public int length() {}
- * }
- */
-
 class Solution {
     public int findInMountainArray(int target, MountainArray mountainArr) {
-        int i=0;
         int n = mountainArr.length();
-        int j = n-1;
+
+        
         int peak = peak(mountainArr);
-        int asc= Asc(mountainArr,peak,target);
-        
-        if(asc>-1){
-            return asc;
-        }else{
 
         
-        return dec(mountainArr,peak,target);
-        }
+        int ascIndex = asc(mountainArr, peak, target);
+        if (ascIndex != -1) return ascIndex;
 
+    
+        return dec(mountainArr, peak + 1, n - 1, target);
     }
 
-    public int peak(MountainArray mountainArr){
-        int i=0;
-        int n = mountainArr.length();
-        int j = n-1;
-
-        
-
-        while(i<j){
-            
-            int mid = i+(j-i)/2;
-            if(mountainArr.get(mid)<mountainArr.get(mid+1)){
-                i=mid+1;
-            }else{
-                j=mid;
+    
+    public int peak(MountainArray mountainArr) {
+        int i = 0, j = mountainArr.length() - 1;
+        while (i < j) {
+            int mid = i + (j - i) / 2;
+            if (mountainArr.get(mid) < mountainArr.get(mid + 1)) {
+                i = mid + 1;
+            } else {
+                j = mid;
             }
         }
-        return j;
+        return i;
     }
 
-    public int Asc(MountainArray mountainArr, int j,int target) {
-
-        int i = 0;
-
+    
+    public int asc(MountainArray mountainArr, int end, int target) {
+        int i = 0, j = end;
         while (i <= j) {
             int mid = i + (j - i) / 2;
-            if (mountainArr.get(mid) < target) {
-                i = mid + 1;
-            } else if (mountainArr.get(mid)> target) {
-                j = mid - 1;
-            } else {
-                return mid;
-            }
+            int val = mountainArr.get(mid);
+            if (val == target) return mid;
+            if (val < target) i = mid + 1;
+            else j = mid - 1;
         }
         return -1;
     }
 
-    public int dec(MountainArray mountainArr, int i,int target){
-        int j=mountainArr.length()-1;
-
-        while (i <= j) {
-            int mid = i + (j - i) / 2;
-            if (mountainArr.get(mid) < target) {
-                i = mid + 1;
-            } else if (mountainArr.get(mid)> target) {
-                j = mid - 1;
-            } else {
-                return mid;
-            }
+    
+    public int dec(MountainArray mountainArr, int start, int end, int target) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            int val = mountainArr.get(mid);
+            if (val == target) return mid;
+            if (val > target) start = mid + 1; // reversed logic
+            else end = mid - 1;
         }
         return -1;
-
     }
-
 }
