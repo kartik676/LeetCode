9c0125bbbc4823a0d6ff62d67ffb18d8a3c89d68
Kartik Maurya <112011936+kartik676@users.noreply.github.com>
2026-09-06 22:21:24 +0530

    LC-search-in-rotated-sorted-array [Runtime: N/A; Memory: N/A]

--- array/search-in-rotated-sorted-array.java
+++ array/search-in-rotated-sorted-array.java
@@ -1,55 +1,26 @@
-
-
 class Solution {
     public int search(int[] nums, int target) {
-        int n = nums.length;
+        int l = 0, r = nums.length - 1;
 
-        // Step 1: Find peak
-        int peak = peak(nums,n);
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
 
-        // Step 2: Search in ascending part
-        int lefthalf = left(nums, peak, target);
-        if (lefthalf != -1) return lefthalf;
+            if (nums[mid] == target) return mid;
 
-        // Step 3: Search in descending part
-        return right(nums, peak + 1, n - 1, target);
-    }
-
-    // Find peak index
-    public int peak(int[] nums,int n) {
-        int i = 0, j = n - 1;
-        while (i < j) {
-            int mid = i + (j - i) / 2;
-            if (nums[mid] < nums[mid + 1]) {
-                i = mid + 1;
-            } else {
-                j = mid;
+            // Left half sorted
+            if (nums[l] <= nums[mid]) {
+                if (nums[l] <= target && target < nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else { // Right half sorted
+                if (nums[mid] < target && target <= nums[r]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
             }
-        }
-        return i;
-    }
-
-    // Binary search on ascending part
-    public int left(int[] nums, int end, int target) {
-        int i = 0, j = end;
-        while (i <= j) {
-            int mid = i + (j - i) / 2;
-            int val = nums[mid];
-            if (val == target) return mid;
-            if (val < target) i = mid + 1;
-            else j = mid - 1;
-        }
-        return -1;
-    }
-
-    // Binary search on descending part
-    public int right(int[] nums, int start, int end, int target) {
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            int val = nums[mid];
-            if (val == target) return mid;
-            if (val < target) start = mid + 1; // reversed logic
-            else end = mid - 1;
         }
         return -1;
     }
