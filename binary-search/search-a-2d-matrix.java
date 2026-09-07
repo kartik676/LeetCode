211b30a96150dbfbcc3e3c7c1c957f55c52ed450
Kartik Maurya <112011936+kartik676@users.noreply.github.com>
2026-09-07 23:30:22 +0530

    LC-search-a-2d-matrix [Runtime: N/A; Memory: N/A]

--- binary-search/search-a-2d-matrix.java
+++ binary-search/search-a-2d-matrix.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
      int r=0;
-     int c=matrix.length-1;
+     int c=matrix[0].length-1;
 
      while(r<matrix.length && c>=0){
             if(matrix[r][c]==target){
